6f2b36a3a3c2ad08281bda964de77717428a6187
Joran Mowry <jordan.mowry@gmail.com>
2023-09-15 12:40:45 -0400

    build reusable table component

--- src/ui/Table.tsx
+++ src/ui/Table.tsx
@@ -1,23 +1,45 @@
-import styled from "styled-components";
+import { createContext, useContext } from 'react';
+import styled from 'styled-components';
+
+interface CommonRowProps extends React.HTMLProps<HTMLDivElement> {
+  $columns?: string;
+}
+
+type TableProps = {
+  columns: string;
+  children: React.ReactNode;
+};
+
+type RowProps = {
+  children: React.ReactNode;
+};
+
+type HeaderProps = RowProps;
+
+type BodyProps = {
+  data: any[];
+  render: (item: any) => React.ReactNode;
+};
 
 const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
-
   font-size: 1.4rem;
   background-color: var(--color-grey-0);
   border-radius: 7px;
   overflow: hidden;
 `;
 
-const CommonRow = styled.div`
+const CommonRow = styled.div<CommonRowProps>`
   display: grid;
-  grid-template-columns: ${(props) => props.columns};
+  grid-template-columns: ${(props) => props.$columns};
   column-gap: 2.4rem;
   align-items: center;
   transition: none;
 `;
 
-const StyledHeader = styled(CommonRow)`
+const StyledHeader = styled(CommonRow).attrs((props) => ({
+  role: 'row',
+}))`
   padding: 1.6rem 2.4rem;
 
   background-color: var(--color-grey-50);
@@ -28,7 +50,9 @@ const StyledHeader = styled(CommonRow)`
   color: var(--color-grey-600);
 `;
 
-const StyledRow = styled(CommonRow)`
+const StyledRow = styled(CommonRow).attrs((props) => ({
+  role: 'row',
+}))`
   padding: 1.2rem 2.4rem;
 
   &:not(:last-child) {
@@ -46,7 +70,6 @@ const Footer = styled.footer`
   justify-content: center;
   padding: 1.2rem;
 
-  /* This will hide the footer when it contains no child elements. Possible thanks to the parent selector :has 🎉 */
   &:not(:has(*)) {
     display: none;
   }
@@ -58,3 +81,55 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContext = createContext<{ $columns: string } | undefined>(undefined);
+
+function Table({ columns, children }: TableProps) {
+  console.log('columns', columns);
+  return (
+    <TableContext.Provider value={{ $columns: columns }}>
+      <StyledTable role='table'>{children}</StyledTable>
+    </TableContext.Provider>
+  );
+}
+
+function Header({ children }: HeaderProps) {
+  const context = useContext(TableContext);
+
+  if (!context) {
+    throw new Error('Header or Row components must be used within a Table');
+  }
+
+  return (
+    <StyledHeader role='row' $columns={context.$columns} as='header'>
+      {children}
+    </StyledHeader>
+  );
+}
+
+function Row({ children }: RowProps) {
+  const context = useContext(TableContext);
+
+  if (!context) {
+    throw new Error('Header or Row components must be used within a Table');
+  }
+
+  return (
+    <StyledRow role='row' $columns={context.$columns}>
+      {children}
+    </StyledRow>
+  );
+}
+
+function Body({ data, render }: BodyProps) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
+
+Table.Header = Header;
+Table.Row = Row;
+Table.Body = Body;
+Table.Footer = Footer;
+
+export default Table;
